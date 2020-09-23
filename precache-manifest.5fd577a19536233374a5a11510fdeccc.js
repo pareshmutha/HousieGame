84ea8505bb3bf6081cd9681212a8e99eaf9f5e9f
Paresh <paresh.mutha@druva.com>
2020-09-23 18:39:45 +0530

    Updates

---
+++ precache-manifest.5fd577a19536233374a5a11510fdeccc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bf953df6022ad33a15bbfba0caa5fa",
+    "url": "/HousieGame/index.html"
+  },
+  {
+    "revision": "c037be7b13187afe68ba",
+    "url": "/HousieGame/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "90fd6ff76829d3a41b62",
+    "url": "/HousieGame/static/css/main.961a5246.chunk.css"
+  },
+  {
+    "revision": "c037be7b13187afe68ba",
+    "url": "/HousieGame/static/js/2.aeb48439.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/HousieGame/static/js/2.aeb48439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fd6ff76829d3a41b62",
+    "url": "/HousieGame/static/js/main.16c42123.chunk.js"
+  },
+  {
+    "revision": "fc3367d497f804f6d778",
+    "url": "/HousieGame/static/js/runtime-main.0f9f9b65.js"
+  },
+  {
+    "revision": "a1784dd2dd390bc5791d6d4729d603ac",
+    "url": "/HousieGame/static/media/phone_bg.a1784dd2.png"
+  },
+  {
+    "revision": "9f3de440534d20dac6eb0d21e9994a3b",
+    "url": "/HousieGame/static/media/play_button.9f3de440.png"
+  },
+  {
+    "revision": "79e0e6dd915c0fc0375d43a9846e1f63",
+    "url": "/HousieGame/static/media/shuffle_button.79e0e6dd.png"
+  }
+]);
